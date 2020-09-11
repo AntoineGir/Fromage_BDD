46abff0cc7ae9034e9c70fa9073109d7ccc004ed
Antoine <antoine.girard@saintmichelannnecy.fr>
2020-09-11 11:54:01 +0200

    Ajoutez des fichiers projet.

---
+++ Fromage_BDD/DBConnect.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Fromage_BDD
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "Fromage";
+            uid = "root";
+            password = "5MichelAnnecy";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE="
+                + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public void Insert(string Query)
+        {
+            string query = Query;
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        public void Update(string Query)
+        {
+            string query = Query;
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        public void Delete(string Query)
+        {
+            string query = Query;
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        public void ExecQuery(string Query)
+        {
+            string query = Query;
+            OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+
+        }
+    }
+}
---
+++ Fromage_BDD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fromage_BDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dbal db = new dbal();
+            
+            
+            db.ExecQuery("INSERT INTO pays (id, nom) VALUES (11, 'savoie' )");
+
+        }
+    }
+}
